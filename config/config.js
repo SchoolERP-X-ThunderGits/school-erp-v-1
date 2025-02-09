c63331981a49a51f4e91e1fcba7a46691894b922
Aakriti Suman <140644983+aakritismn@users.noreply.github.com>
2025-02-09 19:51:01 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ const config = {
 
   // Database configuration
   database: {
-    url: process.env.MONGODB_URI || 'mongodb://localhost:27017/schoolDb', // MongoDB connection URI
+    url: process.env.MONGODB_URI || 'mongodb+srv://thundergits:VSjOjRcjhaF8ohFP@cluster0.z23dt.mongodb.net/', // MongoDB connection URI
   },
 
   // Authentication configuration
